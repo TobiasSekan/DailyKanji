446b2051c45884f5448fb342ae303e30f79011df
Sekan, Tobias <t.sekan@esa-grimma.de>
2018-10-26 07:36:16 +0200

    Extend CommandHelper to make commands more handy

--- DailyKanji/Helper/CommandHelper.cs
+++ DailyKanji/Helper/CommandHelper.cs
@@ -5,17 +5,81 @@ namespace DailyKanji.Helper
 {
     public sealed class CommandHelper : ICommand
     {
-        private readonly Action<object> _action;
+        #region Private Fields
 
-        public CommandHelper(Action<object> action)
-            => _action = action;
+        /// <summary>
+        /// <see cref="Action"/> of this command
+        /// </summary>
+        private readonly Action _action;
 
+        /// <summary>
+        /// <see cref="Action{}"/> of this command
+        /// </summary>
+        private readonly Action<object> _actionWithParameter;
+
+        /// <summary>
+        /// <see cref="Predicate{}"/> of this command indicate that the action can preform
+        /// </summary>
+        private readonly Predicate<bool> _canExcecute;
+
+        /// <summary>
+        /// <see cref="Predicate{}"/> of this command indicate that the action can preform
+        /// </summary>
+        private readonly Predicate<object> _canExcecuteWithParameter;
+
+        #endregion Private Fields
+
+        #region Public Constructors
+
+        /// <summary>
+        /// Create a new command for the given action
+        /// </summary>
+        /// <param name="action">The <see cref="Action"/> for the command</param>
+        /// <param name="canExcecute">(Optional) The <see cref="Predicate{}"/> that indicate that the action can preform</param>
+        public CommandHelper(Action action, Predicate<bool> canExcecute = null)
+        {
+            _action      = action;
+            _canExcecute = canExcecute;
+        }
+
+        /// <summary>
+        /// Create a new command for the given action
+        /// </summary>
+        /// <param name="action">The <see cref="Action{T}"/> for the command</param>
+        /// <param name="canExcecute">(Optional) The <see cref="Predicate{T}"/> that indicate that the action can preform</param>
+        public CommandHelper(Action<object> action, Predicate<object> canExcecute = null)
+        {
+            _actionWithParameter      = action;
+            _canExcecuteWithParameter = canExcecute;
+        }
+
+        #endregion Public Constructors
+
+        #region ICommand Implementation
+
+        /// <summary>
+        /// Execute the <see cref="Action{T}"/> with the given parameter
+        /// </summary>
+        /// <param name="parameter">The parameter for the action</param>
         public void Execute(object parameter)
-            => _action(parameter);
+        {
+            _action?.Invoke();
 
+            _actionWithParameter?.Invoke(parameter);
+        }
+
+        /// <summary>
+        /// Return if the action can perform, based on the <see cref="Predicate{T}"/> of this <see cref="Action{T}"/> and the given parameter
+        /// </summary>
+        /// <param name="parameter"></param>
+        /// <returns></returns>
         public bool CanExecute(object parameter)
-            => true;
+            => _canExcecute != null
+                ? bool.TryParse(parameter.ToString(), out var parameterBool) && _canExcecute(Convert.ToBoolean(parameterBool))
+                : _canExcecuteWithParameter == null || _canExcecuteWithParameter(parameter);
 
         public event EventHandler CanExecuteChanged;
+
+        #endregion ICommand Implementation
     }
 }
--- DailyKanji/Mvvm/ViewModel/MainViewModelCommands.cs
+++ DailyKanji/Mvvm/ViewModel/MainViewModelCommands.cs
@@ -7,11 +7,14 @@ namespace DailyKanji.Mvvm.ViewModel
 {
     public sealed partial class MainViewModel
     {
+        public ICommand NewQuestion
+            => new CommandHelper(CreateNewTest);
+
+        public ICommand CloseProgram
+            => new CommandHelper(() => Environment.Exit(0));
+
         public ICommand AnswerNumber
-            => new CommandHelper((parameter) =>
-            {
-                CheckAnswer(Model.PossibleAnswers.ElementAtOrDefault(Convert.ToInt32(parameter) - 1)?.Roomaji);
-            });
+            => new CommandHelper((parameter) => CheckAnswer(Model.PossibleAnswers.ElementAtOrDefault(Convert.ToInt32(parameter) - 1)?.Roomaji));
 
         public ICommand ChangeAnswerCount
             => new CommandHelper((value) =>
@@ -22,13 +25,10 @@ namespace DailyKanji.Mvvm.ViewModel
             });
 
         public ICommand ChangeAnswerMode
-            => new CommandHelper((_) =>
+            => new CommandHelper(() =>
             {
                 ChooseNewPossibleAnswers();
                 BuildAnswerButtons();
             });
-
-        public ICommand CloseProgram
-            => new CommandHelper((_) => Environment.Exit(0));
     }
 }
