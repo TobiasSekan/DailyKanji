6c36c60c2611ddb925eeb7c40216d024c72a611f
TobiasSekan <tobias.sekan@gmail.com>
2018-12-12 08:17:51 +0100

    Finished work on resize buttons on window width change, but need a little bit more refactoring clean-up

--- DailyKanji/Mvvm/View/MainWindow.xaml.cs
+++ DailyKanji/Mvvm/View/MainWindow.xaml.cs
@@ -1,4 +1,7 @@
 ﻿using DailyKanji.Mvvm.ViewModel;
+using System.Collections.Generic;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 
 namespace DailyKanji.Mvvm.View
 {
@@ -10,12 +13,96 @@ namespace DailyKanji.Mvvm.View
 
         #endregion Public Properties
 
+        #region Internal Properties
+
+        internal IReadOnlyList<TextBlock> _answerTextList { get; }
+
+        internal IReadOnlyList<ButtonBase> _buttonList { get; }
+
+        internal IReadOnlyList<ColumnDefinition> _answerButtonColumn { get; }
+
+        internal IReadOnlyList<TextBlock> _answerHintTextBlock { get; }
+
+        internal IReadOnlyList<TextBlock> _answerShortCutTextBlock { get; }
+
+        #endregion Internal Properties
+
         #region Public Constructors
 
         internal MainWindow(MainViewModel viewModel)
         {
             ViewModel = viewModel;
             InitializeComponent();
+
+            _answerTextList = new[]
+            {
+                AnswerText01,
+                AnswerText02,
+                AnswerText03,
+                AnswerText04,
+                AnswerText05,
+                AnswerText06,
+                AnswerText07,
+                AnswerText08,
+                AnswerText09,
+                AnswerText10
+            };
+
+            _buttonList = new[]
+            {
+                Button01,
+                Button02,
+                Button03,
+                Button04,
+                Button05,
+                Button06,
+                Button07,
+                Button08,
+                Button09,
+                Button10
+            };
+
+            _answerButtonColumn = new[]
+            {
+                AnswerButtonColumn01,
+                AnswerButtonColumn02,
+                AnswerButtonColumn03,
+                AnswerButtonColumn04,
+                AnswerButtonColumn05,
+                AnswerButtonColumn06,
+                AnswerButtonColumn07,
+                AnswerButtonColumn08,
+                AnswerButtonColumn09,
+                AnswerButtonColumn10
+            };
+
+            _answerHintTextBlock = new[]
+            {
+                AnswerHint01,
+                AnswerHint02,
+                AnswerHint03,
+                AnswerHint04,
+                AnswerHint05,
+                AnswerHint06,
+                AnswerHint07,
+                AnswerHint08,
+                AnswerHint09,
+                AnswerHint10
+            };
+
+            _answerShortCutTextBlock = new[]
+            {
+                AnswerKey01,
+                AnswerKey02,
+                AnswerKey03,
+                AnswerKey04,
+                AnswerKey05,
+                AnswerKey06,
+                AnswerKey07,
+                AnswerKey08,
+                AnswerKey09,
+                AnswerKey10
+            };
         }
 
         #endregion Public Constructors
--- DailyKanji/Mvvm/ViewModel/MainViewModel.cs
+++ DailyKanji/Mvvm/ViewModel/MainViewModel.cs
@@ -226,130 +226,52 @@ namespace DailyKanji.Mvvm.ViewModel
         /// </summary>
         internal void BuildAnswerMenuAndButtons()
         {
-            var answerTextList = new TextBlock[]
-            {
-                _mainWindow.AnswerText01,
-                _mainWindow.AnswerText02,
-                _mainWindow.AnswerText03,
-                _mainWindow.AnswerText04,
-                _mainWindow.AnswerText05,
-                _mainWindow.AnswerText06,
-                _mainWindow.AnswerText07,
-                _mainWindow.AnswerText08,
-                _mainWindow.AnswerText09,
-                _mainWindow.AnswerText10
-            };
-
-            var buttonList = new Button[]
-            {
-                _mainWindow.Button01,
-                _mainWindow.Button02,
-                _mainWindow.Button03,
-                _mainWindow.Button04,
-                _mainWindow.Button05,
-                _mainWindow.Button06,
-                _mainWindow.Button07,
-                _mainWindow.Button08,
-                _mainWindow.Button09,
-                _mainWindow.Button10
-            };
-
-            var answerButtonColumn = new ColumnDefinition[]
-            {
-                _mainWindow.AnswerButtonColumn01,
-                _mainWindow.AnswerButtonColumn02,
-                _mainWindow.AnswerButtonColumn03,
-                _mainWindow.AnswerButtonColumn04,
-                _mainWindow.AnswerButtonColumn05,
-                _mainWindow.AnswerButtonColumn06,
-                _mainWindow.AnswerButtonColumn07,
-                _mainWindow.AnswerButtonColumn08,
-                _mainWindow.AnswerButtonColumn09,
-                _mainWindow.AnswerButtonColumn10
-            };
-
             _mainWindow?.Dispatcher?.Invoke(new Action(() =>
             {
-                for(byte answerNumber = 0; answerNumber < 10; answerNumber++)
-                {
-                    if(answerNumber < BaseModel.MaximumAnswers)
-                    {
-                        answerButtonColumn[answerNumber].Width = new GridLength(1, GridUnitType.Star);
-                        answerTextList[answerNumber].Text      = GetAnswerText(answerNumber);
-                        buttonList[answerNumber].Visibility    = Visibility.Visible;
-                    }
-                    else
-                    {
-                        answerButtonColumn[answerNumber].Width = GridLength.Auto;
-                        buttonList[answerNumber].Visibility    = Visibility.Collapsed;
-                    }
-                }
-            }));
-
-            return;
-
-            // TODO
-
-            _mainWindow?.Dispatcher?.Invoke(new Action(() =>
-            {
-                _mainWindow.AnswerButtonArea.Children.Clear();
                 _mainWindow.AnswerMenu.Items.Clear();
 
-                for(byte answerNumber = 0; answerNumber < BaseModel.MaximumAnswers; answerNumber++)
+                for(byte answerNumber = 0; answerNumber < 10; answerNumber++)
                 {
-                    var hintColor = ColorConverter.ConvertFromString(BaseModel.HintTextColor
-                                                                                .ElementAtOrDefault(answerNumber));
-
                     var answerColor = ColorConverter.ConvertFromString(BaseModel.AnswerButtonColor
                                                                                 .ElementAtOrDefault(answerNumber));
 
-                    var stackPanel = new StackPanel();
-                    var buttonText = new TextBlock
-                    {
-                        FontSize = 100 - (5 * BaseModel.MaximumAnswers),
-                        Text = GetAnswerText(answerNumber),
-                        Padding = new Thickness(0, 0, 0, 20),
-                        VerticalAlignment = VerticalAlignment.Center
-                    };
+                    var hintColor = ColorConverter.ConvertFromString(BaseModel.HintTextColor.ElementAtOrDefault(answerNumber));
 
-                    stackPanel.Children.Add(new TextBlock
-                    {
-                        FontSize = 32,
-                        Foreground = new SolidColorBrush((Color)hintColor),
-                        HorizontalAlignment = HorizontalAlignment.Center,
-                        Text = GetAnswerHint(answerNumber)
-                    });
+                    _mainWindow._buttonList[answerNumber].Background          = new SolidColorBrush((Color)answerColor);
+                    _mainWindow._answerHintTextBlock[answerNumber].Foreground = new SolidColorBrush((Color)hintColor);
 
-                    stackPanel.Children.Add(new Button
+                    if(answerNumber < BaseModel.MaximumAnswers)
                     {
-                        Background = new SolidColorBrush((Color)answerColor),
-                        Command = CommandAnswerTest,
-                        CommandParameter = BaseModel.PossibleAnswers.ElementAtOrDefault(answerNumber),
-                        Content = buttonText,
-                        Height = 100,
-                        Margin = new Thickness(5, 0, 5, 0),
-                        Width = (_mainWindow.Width - 20 - (10 * BaseModel.MaximumAnswers)) / BaseModel.MaximumAnswers
-                    });
+                        _mainWindow._answerButtonColumn[answerNumber].Width           = new GridLength(1, GridUnitType.Star);
 
-                    if(BaseModel.ShowAnswerShortcuts)
-                    {
-                        stackPanel.Children.Add(new TextBlock
+                        _mainWindow._buttonList[answerNumber].Visibility              = Visibility.Visible;
+                        _mainWindow._answerShortCutTextBlock[answerNumber].Visibility = Visibility.Visible;
+                        _mainWindow._answerHintTextBlock[answerNumber].Visibility     = Visibility.Visible;
+
+                        _mainWindow._answerTextList[answerNumber].Text                = GetAnswerText(answerNumber);
+                        _mainWindow._answerHintTextBlock[answerNumber].Text           = GetAnswerHint(answerNumber);
+                        _mainWindow._answerShortCutTextBlock[answerNumber].Text       = $"{answerNumber + 1}";
+
+                        _mainWindow.AnswerMenu.Items.Add(new MenuItem
                         {
-                            FontSize = 12,
-                            HorizontalAlignment = HorizontalAlignment.Center,
-                            Text = $"{answerNumber + 1}"
+                            Command          = CommandAnswerTest,
+                            CommandParameter = BaseModel.PossibleAnswers.ElementAtOrDefault(answerNumber),
+                            Header           = GetAnswerText(answerNumber),
+                            InputGestureText = $"{answerNumber + 1}"
                         });
                     }
-
-                    _mainWindow.AnswerButtonArea.Children.Add(stackPanel);
-
-                    _mainWindow.AnswerMenu.Items.Add(new MenuItem
+                    else
                     {
-                        Command = CommandAnswerTest,
-                        CommandParameter = BaseModel.PossibleAnswers.ElementAtOrDefault(answerNumber),
-                        Header = GetAnswerText(answerNumber),
-                        InputGestureText = $"{answerNumber + 1}"
-                    });
+                        _mainWindow._answerButtonColumn[answerNumber].Width           = GridLength.Auto;
+
+                        _mainWindow._buttonList[answerNumber].Visibility              = Visibility.Collapsed;
+                        _mainWindow._answerShortCutTextBlock[answerNumber].Visibility = Visibility.Collapsed;
+                        _mainWindow._answerHintTextBlock[answerNumber].Visibility     = Visibility.Collapsed;
+
+                        _mainWindow._answerTextList[answerNumber].Text                = string.Empty;
+                        _mainWindow._answerHintTextBlock[answerNumber].Text           = string.Empty;
+                        _mainWindow._answerShortCutTextBlock[answerNumber].Text       = string.Empty;
+                    }
                 }
             }));
         }
