b66fbc21f2949e04906d7f5975266634599de0f0
Sekan, Tobias <sekan@efa-leipzig.com>
2019-08-13 07:47:44 +0200

    Add enum for hint show type

---
+++ DailyKanjiLogic/Enumerations/HintShowType.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace DailyKanjiLogic.Enumerations
+{
+    /// <summary>
+    /// The kind of hints that should show on wrong/right answers
+    /// </summary>
+    [Flags]
+    public enum HintShowType : byte
+    {
+        /// <summary>
+        /// Show no hint
+        /// </summary>
+        ShowOnNoAnswers = 0x_00,
+
+        /// <summary>
+        /// Show the hint on the wrong answer
+        /// </summary>
+        ShowOnWrongAnswer = 0x_01,
+
+        /// <summary>
+        /// Show the hint on the right answer
+        /// </summary>
+        ShowOnRightAnswer = 0x_02,
+
+        /// <summary>
+        /// Show the hint on all marked answers
+        /// </summary>
+        ShowOnMarkedAnswers = 0x_04,
+
+        /// <summary>
+        /// Show the hint on all other answers (not marked answers, not right, not wrong)
+        /// </summary>
+        ShowOnOtherAnswers = 0x_08
+    }
+}
